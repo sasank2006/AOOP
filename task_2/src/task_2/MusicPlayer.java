55919cb1d89d457d8eb0e6d9a66d824e68f1f59e
sasank2006 <145980344+sasank2006@users.noreply.github.com>
2025-01-07 13:46:10 +0530

    Delete task_2 directory

--- task_2/src/task_2/MusicPlayer.java
+++
@@ -1,5 +0,0 @@
-package task_2;
-
-public interface MusicPlayer {
-    void playMusic(String sourceDetails);
-}
--- task_2/src/task_2/MusicSource.java
+++
@@ -1,6 +0,0 @@
-package task_2;
-
-public interface MusicSource {
-    String getSourceName();
-    String getSourceDetails();
-}
