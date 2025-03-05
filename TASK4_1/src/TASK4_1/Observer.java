6887884649e2780c3829e1207b72ab59cf77cda4
sasank2006 <145980344+sasank2006@users.noreply.github.com>
2025-03-05 08:59:47 +0530

    Delete TASK4_1 directory

--- TASK4_1/src/TASK4_1/Observer.java
+++
@@ -1,5 +0,0 @@
-package TASK4_1;
-
-public interface Observer {
-    void update(String event);
-}
--- TASK4_1/src/TASK4_1/Subject.java
+++
@@ -1,7 +0,0 @@
-package TASK4_1;
-
-public interface Subject {
-    void addObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObservers(String event);
-}
